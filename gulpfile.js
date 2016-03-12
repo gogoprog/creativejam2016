b0eaf53288b28943bb535687c492ae7851fa5102
PieterVantorre <pietervantorre@gmail.com>
2016-03-12 15:54:34 +0100

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./**/*.scss', ['sass']);
 });
