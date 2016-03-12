9e39847dbc1b1539b476663346b441d5bdf95a09
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-03-12 14:00:05 +0100

    wip

--- src/backend/game.js
+++ src/backend/game.js
@@ -9,12 +9,18 @@ fs.readFile(__dirname + '/words.json', 'utf8', function (err,data) {
     words = JSON.parse(data);
 });
 
+let State = {
+    WAITING_FOR_PLAYERS:0,
+    WAITING_FOR_WORDS:1
+};
+
 class Game {
     constructor(io, name)
     {
         this.io = io;
         this.name = name;
         this.players = [];
+        this.state = State.WAITING_FOR_PLAYERS;
     }
 
     emit(e, data)
@@ -26,7 +32,34 @@ class Game {
     {
         socket.join(this.name);
         this.players.push(socket);
-        this.emit('join', "player join");
+        this.emit('join', "player joined " + this.name);
+
+        socket.index = this.players.length - 1;
+
+        if(this.state == State.WAITING_FOR_PLAYERS && this.players.length > 1)
+        {
+            this.start();
+        }
+
+        let that = this;
+        socket.on('word', function(data) {
+            that.onWord(socket, data);
+        });
+    }
+
+    start()
+    {
+        this.currentWord = words.en[0];
+        this.playerWords = Array(this.players.length);
+        for(var p in this.players) this.players[p] = null;
+        this.emit('start', this.currentWord);
+        this.state = State.WAITING_FOR_WORDS;
+    }
+
+    onWord(socket, word)
+    {
+        console.log(word, 'from', socket.index);
+        this.playerWords[socket.index] = word;
     }
 }
 
