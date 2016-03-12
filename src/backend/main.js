557c289ee12d26d33be20b5203dafa49ec2fbd30
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-03-12 11:35:39 +0100

    Added test

--- src/backend/main.js
+++ src/backend/main.js
@@ -3,8 +3,15 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function(){ /* … */ });
+io.on('connection', function(socket) {
+    console.log("Client connected.");
+    socket.on('join', function(roomName) {
+        socket.broadcast.to(roomName).emit('join', "player join");
+    });
+});
+
 
 app.use(express.static(__dirname + '/../frontend'));
+app.use("/socket.io/", express.static(__dirname + '../../node_modules/socket.io-client/'));
 
 server.listen(process.env.PORT || 8000);
