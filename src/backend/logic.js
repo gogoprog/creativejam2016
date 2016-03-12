d31d7327b08004ef68634b60b47543d479b3d162
PieterVantorre <pietervantorre@gmail.com>
2016-03-12 12:29:25 +0100

    added logic to calculate score

---
+++ src/backend/logic.js
@@ -0,0 +1,28 @@
+var ParsedScore = null;
+
+function initialize()
+{
+    ParsedScore = JSON.parse("/common/score.json");
+}
+
+function calculateStringScore( string ) {
+    let i = string.length;
+    let score = 0;
+
+    calculateCharacterScore = function( char ) {
+        if ( ParsedScore.hasOwnProperty(char) ) {
+            return ParsedScore[char];
+        }
+
+        return 0;
+    };
+
+    while (i--) {
+      score += calculateCharacterScore( string.charAt(i) );
+    }
+
+    return score;
+}
+
+module.exports.initialize = initialize;
+module.exports.calculateStringScore = calculateStringScore;
--- src/backend/main.js
+++ src/backend/main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var logic = require('./logic');
 
 io.on('connection', function(socket) {
     console.log("Client connected.");
@@ -14,5 +15,8 @@ io.on('connection', function(socket) {
 app.use(express.static(__dirname + '/../frontend'));
 app.use("/socket.io/", express.static(__dirname + '/../../node_modules/socket.io-client/'));
 app.use("/bootstrap/", express.static(__dirname + '/../../node_modules/bootstrap/dist/'));
+app.use("/common/", express.static(__dirname + '/../common/'));
 
-server.listen(process.env.PORT || 8000);
+server.listen(process.env.PORT || 8000, function() {
+    logic.initialize();
+});
